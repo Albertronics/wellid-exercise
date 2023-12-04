5c34d1d1a55a9829b6d012d7c23c6a9203e2dc3d
Alberto Musciumarra <albertomusciumarra@gmail.com>
2023-12-04 22:07:41 +0100

    feat: implement Line class

---
+++ src/main/java/com/wellid/exercise/common/Fraction.java
@@ -0,0 +1,41 @@
+package com.wellid.exercise.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Fraction
+{
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public double toDouble() {
+        return (double) numerator / denominator;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Fraction fraction = (Fraction) o;
+        return fraction.denominator == denominator && fraction.numerator == numerator;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + numerator;
+        result = prime * result + denominator;
+
+        return result;
+    }
+}
---
+++ src/main/java/com/wellid/exercise/models/Line.java
@@ -0,0 +1,41 @@
+package com.wellid.exercise.models;
+
+import com.wellid.exercise.common.Fraction;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+public class Line
+{
+    private Fraction slope;
+    private double intercept;
+
+    public Line(Fraction slope, double intercept) {
+        this.slope = slope;
+        this.intercept = intercept;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+
+        return Double.compare(line.intercept, intercept) == 0 && Objects.equals(slope, line.slope);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + ((slope == null) ? 0 : slope.hashCode());
+        long interceptBits = Double.doubleToLongBits(intercept);
+        result = prime * result + (int) (interceptBits ^ (interceptBits >>> 32));
+
+        return result;
+    }
+}
