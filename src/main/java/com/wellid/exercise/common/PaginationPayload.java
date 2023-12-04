5545637ccb55020128b0aa19c359e90904d6e6d4
Alberto Musciumarra <albertomusciumarra@gmail.com>
2023-12-04 17:17:50 +0100

    feat: add PaginationPayload common POJO

---
+++ src/main/java/com/wellid/exercise/common/PaginationPayload.java
@@ -0,0 +1,22 @@
+package com.wellid.exercise.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PaginationPayload
+{
+    private Integer page;
+    private Integer size;
+
+    public PaginationPayload(Integer page, Integer size) {
+        this.page = page;
+        this.size = size;
+    }
+
+    public PaginationPayload() {
+        this.page = 0;
+        this.size = 25;
+    }
+}
