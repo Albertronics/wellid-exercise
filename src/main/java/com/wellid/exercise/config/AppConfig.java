233af14cb65e3248df2cb45e4ec6ab412235d252
Alberto Musciumarra <albertomusciumarra@gmail.com>
2023-12-04 17:21:25 +0100

    fix: add missing messageSource bean

---
+++ src/main/java/com/wellid/exercise/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.wellid.exercise.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class AppConfig
+{
+    @Bean
+    public MessageSource messageSource() {
+        var messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(-1);
+
+        return messageSource;
+    }
+}
