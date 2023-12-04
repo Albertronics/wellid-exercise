ff2b51f0373eb2c3a5a5ab93465655b4549c4506
Alberto Musciumarra <albertomusciumarra@gmail.com>
2023-12-04 22:07:14 +0100

    feat: implement helper methods for PointEntity

--- src/main/java/com/wellid/exercise/entities/PointEntity.java
+++ src/main/java/com/wellid/exercise/entities/PointEntity.java
@@ -5,12 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity(name = "points")
 @Accessors(chain = true)
@@ -31,5 +34,30 @@ public class PointEntity
     public static class PointEntityId implements Serializable {
         private Integer x;
         private Integer y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d, %d]", this.x, this.y);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PointEntity point = (PointEntity) o;
+        return point.x == x && point.y == y;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + x;
+        result = prime * result + y;
+
+        return result;
     }
 }
